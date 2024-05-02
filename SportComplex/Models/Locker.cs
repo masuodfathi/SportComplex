5301099859fbddcfa8b531b478ce81fb1a2d8b35
A. Taghavi <taghavizadeh.alireza@gmail.com>
2024-05-02 20:51:13 +0330

    feat: Add Locker class

---
+++ SportComplex/Models/Locker.cs
@@ -0,0 +1,11 @@
+﻿namespace SportComplex.Models;
+
+public class Locker
+{
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public bool IsAvailable { get; set; }
+        public string AssignedTo { get; set; }
+        public DateTime ReservationStartTime { get; set; }
+        public DateTime ReservationEndTime { get; set; }
+}
