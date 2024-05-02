27e0db91a971da0a0b979ce2fa964ae43e263116
A. Taghavi <taghavizadeh.alireza@gmail.com>
2024-05-02 20:56:25 +0330

    feat: Add Storage class

---
+++ SportComplex/Models/Storage.cs
@@ -0,0 +1,9 @@
+﻿namespace SportComplex.Models;
+
+public class Storage
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Capacity { get; set; }
+    public List<string> Items { get; set; }
+}
