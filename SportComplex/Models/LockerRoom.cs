a045a33dc520673e07348f5329d5f4c95bc35d70
A. Taghavi <taghavizadeh.alireza@gmail.com>
2024-05-02 20:51:24 +0330

    feat: Add LockerRoom class

---
+++ SportComplex/Models/LockerRoom.cs
@@ -0,0 +1,9 @@
+﻿namespace SportComplex.Models;
+
+public class LockerRoom
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Capacity { get; set; }
+    public Locker[] Lockers { get; set; }
+}
