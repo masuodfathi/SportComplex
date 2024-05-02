7355df4184e6bef9bbce9378cd710aa1448b4ece
A. Taghavi <taghavizadeh.alireza@gmail.com>
2024-05-02 20:53:54 +0330

    feat: Add Athlete class

---
+++ SportComplex/Models/Athlete.cs
@@ -0,0 +1,15 @@
+﻿namespace SportComplex.Models;
+
+public class Athlete
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Gender { get; set; }
+    public string Sport { get; set; }
+    public double Height { get; set; }
+    public double Weight { get; set; }
+}
