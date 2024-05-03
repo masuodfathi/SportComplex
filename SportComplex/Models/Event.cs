6e5cf51ad9a79b5a9e4c96811122586753799924
Bezyeight <bezy4ever@gmail.com>
2024-05-03 03:54:08 -0700

    Create Event Class

---
+++ SportComplex/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿namespace SportComplex.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name {  get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate {  get; set; }
+        public DateTime EndDate { get; set; }
+        public string Location {  get; set; }
+        public bool IsOutdoor {  get; set; }
+        public string OrganizerName {  get; set; }
+        public int Capacity {  get; set; }
+    }
+}
