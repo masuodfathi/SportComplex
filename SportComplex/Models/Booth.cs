c18f92a5a1e8855264e0110997e71f127670a099
Bezyeight <bezy4ever@gmail.com>
2024-05-03 11:38:42 -0700

    Add Ticket Class/Add SportEquipment Class/Add Booth Class

---
+++ SportComplex/Models/Booth.cs
@@ -0,0 +1,13 @@
+﻿namespace SportComplex.Models
+{
+    public class Booth
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsOperational { get; set; }
+        public string Type { get; set; }
+        public string Location { get; set; }
+
+    }
+}
---
+++ SportComplex/Models/SportEquipment.cs
@@ -0,0 +1,13 @@
+﻿namespace SportComplex.Models
+{
+    public class SportEquipment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Brand { get; set; }
+        public bool IsAvailable { get; set; }
+
+    }
+}
