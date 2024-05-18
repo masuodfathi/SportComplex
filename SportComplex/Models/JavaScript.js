e02b93780227a02f4defc0e26134558b897f9ee2
Amirreza <2561amir@gmail.com>
2024-05-18 16:42:40 +0330

    Add View For Home Page - Add New Layout - Add Required css and js Files - Add Required images // SalehAbadi Amirreza

---
+++ SportComplex/Models/JavaScript.js
@@ -0,0 +1,49 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById('contact-form');
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('message').value;
+
+        alert(`نام: ${name}\nایمیل: ${email}\nپیام: ${message}\nپیام شما با موفقیت ارسال شد!`);
+    });
+
+
+
+
+    // Dashboard Toggle
+    const toggleButton = document.querySelector('.dashboard-toggle');
+    const dashboardContent = document.querySelector('.dashboard-content');
+
+    toggleButton.addEventListener('click', function () {
+        if (dashboardContent.style.display === 'block') {
+            dashboardContent.style.display = 'none';
+        } else {
+            dashboardContent.style.display = 'block';
+        }
+    });
+});
+
+
+
+
+
+
+document.getElementById('navbar-toggle').addEventListener('click', function () {
+    const navbarMenu = document.getElementById('navbar-menu');
+    navbarMenu.classList.toggle('active');
+});
+
+
+
+
+
+var modal = document.getElementById('id01');
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
